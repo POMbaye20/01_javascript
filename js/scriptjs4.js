4a6a8029c619ce96d674a99d9f41221f2ea9c97c
Papaoumar <39194180+POMbaye20@users.noreply.github.com>
2018-06-18 17:00:39 +0200

    commit 02
    c 02

---
+++ js/scriptjs4.js
@@ -0,0 +1,35 @@
+//== comparer que 2 valeurs sont identiques (ex: 7 et "7")
+// === vérification que 2 valeurs sont identiques et du même type (ex: 7 et 7(vrai) | ex : 7 et "7" (faux))
+// < inférieur à 
+// > supérieur à
+// >= supérieur ou égal à
+// <= inférieur ou égal
+// != vérifier que 2 valeurs sont inégales
+// !== vérifier que 2 valeurs sont strictement inégales  
+
+// Opérateur logique 
+// && -> AND (et)
+// || -> OR (OU)
+// * -> NOT (non)
+
+// var t = "quelqueschose";
+// var r = 12;
+// // Si t est identiques et de même type que "quelquechose" et que r est égal et du même que 1 on écrit "quelquechose" sinon on écrit r.
+
+// if ((t === "quelquechose") && (r === 1)) {
+//     document.write(t);//Ne s'exécute que si les 2 conditions sont vraies.
+// }else{
+//     document.write(r);//Ne s'exécute que si l'une des conditions est fausse.
+// }
+// //Si t est identique et du même type que "quelquechose" ou que r est égale et du même type que number on écrit r.
+// if ((t === "quelquechose") || (r === number)) {
+//     document.write(r)
+// }
+// document.write("Erreur !!!!");
+var p = prompt ("test");
+
+if (!t === "test") {
+    document.write('azerty');
+}else{
+    document.write('poiuytreza');
+}
